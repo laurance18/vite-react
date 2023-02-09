0bba01167580505384c69395c094c57cd0b98598
Deniz Işık <denizisik2004@gmail.com>
2023-02-09 15:18:45 +0300

    Remove import statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar.jsx'
-import Main from'./components/Main.jsx'
 import Footer from'./components/Footer.jsx'
 
 function App() {
