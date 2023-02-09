cd7e5631640c8d497d016b4a7db197929e5e9266
Deniz Işık <denizisik2004@gmail.com>
2023-02-09 14:45:18 +0300

    Fix nav selector causing confusion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 
 export default function Navbar() {
     return (
-        <nav>
+        <nav className="navbar">
             <img src={logo} className="nav--icon"/>
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
