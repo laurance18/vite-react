46e2cd148465a65a4100ebfa459dbfa1ec435f4b
Deniz Işık <denizisik2004@gmail.com>
2023-02-09 15:09:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Main from'./components/Main.jsx'
+import Footer from'./components/Footer.jsx'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Main />
+      <Footer />
     </div>
   )
 }
