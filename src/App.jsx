4b07b784a531b303e9030603576dc25b65b7b510
Deniz Işık <denizisik2004@gmail.com>
2023-03-01 22:24:53 +0300

    Move react_info_site to necessary folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Main from './components/Main.jsx'
-import Navbar from './components/Navbar.jsx'
+import Main from './react_info_site/components/Main.jsx'
+import Navbar from './react_info_site/components/Navbar.jsx'
 
 function App() {
   return (
