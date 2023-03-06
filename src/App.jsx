4c3d1819e41fa7efc52473145063bcad5d31de20
Deniz Işık <denizisik2004@gmail.com>
2023-03-06 13:59:35 +0300

    Adjust for new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Main from './react_info_site/components/Main.jsx'
-import Navbar from './react_info_site/components/Navbar.jsx'
+import Navbar from './airbnb_clone/components/Navbar.jsx'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Main />
     </div>
   )
 }
