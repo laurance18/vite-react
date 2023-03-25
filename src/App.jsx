807376f18f3204584b7b0d15c08999c05a31a24e
Deniz Işık <denizisik2004@gmail.com>
2023-03-25 17:22:50 +0300

    init hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './airbnb_clone/components/Navbar.jsx'
+import Hero from './airbnb_clone/components/Hero.jsx'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   )
 }
