fcbb502f9e9db3dfbae2c94bb1706848ee17b3e1
Deniz Işık <denizisik2004@gmail.com>
2023-03-25 17:42:28 +0300

    Complete hero comp.

--- src/airbnb_clone/components/Hero.jsx
+++ src/airbnb_clone/components/Hero.jsx
@@ -1,10 +1,13 @@
 import './Hero.css'
-import photo_grid from '../assests/photo_grid.png'
+import photo_grid from '../assests/photo-grid.png'
 
 export default function Hero() {
     return(
-        <>
-            
-        </>
+        <section className="hero">
+            <img src={photo_grid} className="hero--photo" />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
     )
 }
