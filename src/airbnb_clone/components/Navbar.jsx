1a41497f31a350be688966ba155ba89d19660b42
Deniz Işık <denizisik2004@gmail.com>
2023-03-25 17:22:42 +0300

    Complete nav comp.

--- src/airbnb_clone/components/Navbar.jsx
+++ src/airbnb_clone/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../assests/airbnb-logo.png'
 export default function Navbar() {
     return (
         <nav>
-            <img src={logo} className="w-20 pl-8 pt-6"/>
+            <img src={logo} className="nav--logo"/>
         </nav>
     )
 }
