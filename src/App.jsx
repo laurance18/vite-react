84c5f39dc79af97ece3bcca262959d80caf3841e
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 10:55:50 +0300

    Finish card component, first rev.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './airbnb_clone/components/Navbar.jsx'
 import Hero from './airbnb_clone/components/Hero.jsx'
+import Card from './airbnb_clone/components/Card'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Card />
     </div>
   )
 }
