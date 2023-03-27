51ce456af26f28a607bebebc1e9f081b2e3faf2a
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 13:12:03 +0300

    Polish props

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,7 @@ function App() {
         console.log(item.coverImg)
         return (<Card 
             key={item.id}
-            img={item.coverImg}
-            rating={item.stats.rating}
-            reviewCount={item.stats.reviewCount}
-            location={item.location}
-            title={item.title}
-            price={item.price}
-            openSpots={item.openSpots}
+            item={item}
         />)
     })
   
