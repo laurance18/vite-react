9ae22afd211b5ba1847962117af76f165e57bbb1
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 13:24:22 +0300

    Copy legacy project to respective folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './airbnb_clone/components/Navbar.jsx'
-import Hero from './airbnb_clone/components/Hero.jsx'
-import Card from './airbnb_clone/components/Card'
-import data from './airbnb_clone/data.js'
+
 
 function App() {
-    const cards = data.map(item => {
-        console.log(item.coverImg)
-        return (<Card 
-            key={item.id}
-            {...item}
-        />)
-    })
-  
     return (
         <div className="App">
-        <Navbar />
-        <Hero />
-        <section className="cards-list">
-            {cards}
-        </section>
+        
         </div>
     )
 }
