69d6f59aa1c0441408d15dd418867245774103fc
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 17:30:56 +0300

    Finish Hero & Card comp using .map func.

--- src/travel_journal/components/Hero.jsx
+++ src/travel_journal/components/Hero.jsx
@@ -1,7 +1,18 @@
-export default function Hero() {
-    return (
-        <div>
+import Card from "./Card.jsx"
+import data from '../data.js'
 
+export default function Hero() {
+    const cards = data.map(item => {
+        console.log(item.coverImg)
+        return (<Card 
+            key={item.id}
+            {...item}
+        />)
+    }) 
+    
+    return (
+        <div className="bg-gradient-to-r from-gray-900 to-gray-700">
+            {cards}
         </div>
     )
 }
---
+++ src/travel_journal/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        location: 'Dolomite Mountains',
+        country: 'Italy',
+        mapLink: 'https://goo.gl/maps/FbnxAAH83spsEXdA6',
+        dateFrom: '20 May, 2022',
+        dateTo: '26 May, 2022',
+        description: 'The Dolomites, also known as the Dolomite Mountains, Dolomite Alps or Dolomitic Alps, are a mountain range in northeastern Italy. They form part of the Southern Limestone Alps and extend from the River Adige in the west to the Piave Valley (Pieve di Cadore) in the east.',
+        imgURL: 'https://images.unsplash.com/photo-1679673177212-8a011a4f86f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
+    },
+    {
+        id: 2,
+        location: 'Col de la Madeleine',
+        country: 'France',
+        mapLink: 'https://goo.gl/maps/fsEJ2PXeB9Nb3Ym46',
+        dateFrom: '5 January, 2023',
+        dateTo: '12 January, 2023',
+        description: 'Col de la Madeleine (el. 1,993 m.) is a high mountain pass in the Alps in the department of Savoie in France which connects La Chambre in Maurienne with La Léchère in Tarentaise. The pass is closed from November to the beginning of June. It has been described as "beautiful, but heartbreaking".',
+        imgURL: 'https://images.unsplash.com/photo-1679512598221-3454804a8bd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        id: 3,
+        location: 'Luleå',
+        country: 'Sweden',
+        mapLink: 'https://goo.gl/maps/kWJ4vJKyNY9grstN7',
+        dateFrom: '15 March, 2023',
+        dateTo: '22 March, 2023',
+        description: 'Luleå, is a city on the coast of northern Sweden, and the capital of Norrbotten County, the northernmost county in Sweden.',
+        imgURL: 'https://images.unsplash.com/photo-1676496307700-d118f330fd5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1987&q=80'
+    }
+]
