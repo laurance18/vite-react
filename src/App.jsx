13993db2af2742d13cad357a16e5c3b64bff5dfe
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 13:13:27 +0300

    Revert "Polish props"
    This reverts commit 51ce456af26f28a607bebebc1e9f081b2e3faf2a.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,13 @@ function App() {
         console.log(item.coverImg)
         return (<Card 
             key={item.id}
-            item={item}
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+            openSpots={item.openSpots}
         />)
     })
   
--- src/airbnb_clone/components/Card.jsx
+++ src/airbnb_clone/components/Card.jsx
@@ -1,25 +1,17 @@
 import './Card.css'
 
 export default function Card(props) {
-    console.log(props.item)
-    let badgeText
-    if (props.item.openSpots === 0) {
-        badgeText = "SOLD OUT"
-    } else if (props.item.location === "Online") {
-        badgeText = "ONLINE"
-    }
     return(
         <div className="card">
-            <img src={`/assets/airbnb/${props.item.coverImg}`} className="card--image" />
-            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`/assets/airbnb/${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src='assets/airbnb/star.png' className="card--star" />
-                <span>{props.item.stats.rating}</span>
-                <span className="gray">({props.item.stats.reviewCount}) •</span>
-                <span className="gray">{props.item.location}</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) •</span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p className="card--title">{props.item.title}</p>
-            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
