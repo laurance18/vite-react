f69717fdfa7378d2c81843906ca0288158abb378
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 13:18:24 +0300

    Fix in accordance with spread operator

--- src/airbnb_clone/components/Card.jsx
+++ src/airbnb_clone/components/Card.jsx
@@ -1,13 +1,21 @@
 import './Card.css'
 
 export default function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
     return(
         <div className="card">
-            <img src={`/assets/airbnb/${props.img}`} className="card--image" />
+            <img src={`/assets/airbnb/${props.coverImg}`} className="card--image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <div className="card--stats">
                 <img src='assets/airbnb/star.png' className="card--star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) •</span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) •</span>
                 <span className="gray">{props.location}</span>
             </div>
             <p className="card--title">{props.title}</p>
