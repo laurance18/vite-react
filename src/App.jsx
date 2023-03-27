b5e064bd95773b881aba08dafc118e1fdfa6b73f
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 12:55:45 +0300

    Fix indentation

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,29 @@ import Card from './airbnb_clone/components/Card'
 import data from './airbnb_clone/data.js'
 
 function App() {
-  const cards = data.map(item => {
-    console.log(item.coverImg)
-    return (<Card 
-      img={item.coverImg}
-      rating={item.stats.rating}
-      reviewCount={item.stats.reviewCount}
-      location={item.location}
-      title={item.title}
-      price={item.price}
-    />)
-  })
+    const cards = data.map(item => {
+        console.log(item.coverImg)
+        return (<Card 
+            key={item.id}
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+            openSpots={item.openSpots}
+        />)
+    })
   
-  return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-      <section className="cards-list">
-        {cards}
-      </section>
-    </div>
-  )
+    return (
+        <div className="App">
+        <Navbar />
+        <Hero />
+        <section className="cards-list">
+            {cards}
+        </section>
+        </div>
+    )
 }
 
 export default App
