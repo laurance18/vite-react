a5d651fc6d0999dc4dc36f5e95521ef9aa54d343
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 15:19:36 +0300

    Complete navbar comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './travel_journal/components/Navbar'
 
 
 function App() {
     return (
         <div className="App">
-        
+            <Navbar />
         </div>
     )
 }
--- src/travel_journal/components/Navbar.jsx
+++ src/travel_journal/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import './Navbar.css'
-
 export default function Navbar() {
     return (
-        <nav>
-            
+        <nav className="relative shadow bg-gradient-to-b from-red-700 to-red-500 w-full">
+            <div className="relative h-16 mx-auto max-w-7xl flex items-center justify-center text-white">
+                <img src="/world.png" alt="world_png" className="h-10 pr-2 brightness-11" />
+                <span className="text-2xl" href="">Lau's Travel Journal</span>
+            </div>
         </nav>
     )
 }
