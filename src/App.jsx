ebdc1107b2fa4a19aae8870c6701402c4bfd4348
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 17:45:51 +0300

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './travel_journal/components/Navbar'
 import Hero from './travel_journal/components/Hero'
-
+import Footer from './travel_journal/components/Footer'
 
 function App() {
     return (
         <div className="App">
             <Navbar />
             <Hero />
+            <Footer />
         </div>
     )
 }
--- src/travel_journal/components/Footer.jsx
+++ src/travel_journal/components/Footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return(
-        <footer>
-            
+        <footer className="bg-gradient-to-r from-gray-900 to-gray-700 flex justify-center items-center gap-3 pb-4">
+            <a href="https://github.com/laurance18" className="duration-700 hover:rotate-[360deg] hover:scale-110"><img src="/githubLogo.png" alt="github" /></a> 
+            <p className="text-gray-200 text-sm">Made with ❤️ by Laurance18</p>
         </footer>
     )
 }
