1f9a52a0e0f8d18f041031c422287c168b6d47ea
Deniz Işık <denizisik2004@gmail.com>
2023-03-27 15:29:30 +0300

    Update Navbar.jsx

--- src/travel_journal/components/Navbar.jsx
+++ src/travel_journal/components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
         <nav className="relative shadow bg-gradient-to-b from-red-700 to-red-500 w-full">
             <div className="relative h-16 mx-auto max-w-7xl flex items-center justify-center text-white">
                 <img src="/world.png" alt="world_png" className="h-10 pr-2 brightness-11" />
-                <span className="text-2xl" href="">Lau's Travel Journal</span>
+                <a className="text-2xl hover:scale-[1.02] hover:shadow-sm" href="">Lau's Travel Journal</a>
             </div>
         </nav>
     )
