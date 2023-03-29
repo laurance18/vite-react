97843ac417468e6da7f3541d320e8fc61acc8f5a
Deniz Işık <denizisik2004@gmail.com>
2023-03-29 09:27:18 +0300

    init navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
+import Navbar from './meme_generator/components/Navbar'
 
 function App() {
     return (
         <div className="App">
-
+            <Navbar />
         </div>
     )
 }
