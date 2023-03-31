67e64f215985ef3bdbb0c2c73de6d5abc81c0de0
Deniz Işık <denizisik2004@gmail.com>
2023-03-31 16:54:52 +0300

    Finish Meme comp (fetch .then)

--- src/meme_generator/components/Meme.jsx
+++ src/meme_generator/components/Meme.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Meme.css';
 import memesData from '../memesData';
 
@@ -20,13 +20,18 @@ export default function Meme() {
         })
     }
     
-    const [allMemeImages, setAllMemeImages] = useState(memesData)
+    const [allMemes, setAllMemes] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
     
     
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
