3605907007b8ac3f6218f4fcc93e331407ecbd09
Deniz Işık <denizisik2004@gmail.com>
2023-05-29 18:00:06 +0300

    Relocate meme_generator app.jsx

---
+++ src/meme_generator/App.jsx
@@ -0,0 +1,18 @@
+// MOVE TO SRC FOLDER BEFORE USING
+
+import { useState } from 'react'
+import './App.css'
+import Header from './meme_generator/components/Header'
+import Meme from './meme_generator/components/Meme'
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Meme />
+        </div>
+    )
+}
+
+export default App
