a0d9b8fb2209ca55f4f75dd77b8b791784c5b30f
Deniz Işık <denizisik2004@gmail.com>
2023-05-29 17:54:56 +0300

    Fresh files for Tenzies project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './react_info_site/components/Navbar'
-import Main from './react_info_site/components/Main'
+
 
 function App() {
     return (
         <div className="App">
-            <Navbar />
-            <Main />
+
         </div>
     )
 }
