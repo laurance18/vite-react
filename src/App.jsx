c9767f6f39a37850c814c6c6569e262954e74bef
Deniz Işık <denizisik2004@gmail.com>
2023-06-01 19:39:10 +0300

    Add confetti element

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Die from './tenzies/components/Die'
 import {nanoid} from "nanoid"
-import {Confetti} from 'react-confetti'
+import Confetti from 'react-confetti'
 
 function App() {
     
@@ -14,7 +14,6 @@ function App() {
         const allDiceSameValue = dice.every(die => die.value === dice[0].value)
         if (allDiceHeld && allDiceSameValue) {
             setTenzies(true)
-            alert("You won!")
         }         
     }, [dice])
     
@@ -68,12 +67,13 @@ function App() {
     return (
         <div className="App">
             <main>
+                {tenzies && <Confetti height={400}/>}
                 <h1 className="title">Tenzies</h1>
                 <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
                 <div className="dice-container">
                     {diceElements}
                 </div>
-                <button className="roll-dice" onClick={rollDice}>Roll</button>
+                <button className="roll-dice" onClick={rollDice}>{tenzies ? "New Game" : "Roll Dice"}</button>
             </main>
         </div>
     )
