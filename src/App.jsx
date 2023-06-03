50c07a8debcd512d1aad48dfc2f10a985004463b
Deniz Işık <denizisik2004@gmail.com>
2023-06-04 00:32:59 +0300

    Remove getQuestions.jsx, move function into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
     
-    const [gameStarted, setGameStarted] = useState(false)
     
+    const [gameStarted, setGameStarted] = useState(false)
+    const [questions, setQuestions] = useState([])
+
+    useEffect(() => {
+        async function getQuestions() {
+            const res = await fetch("https://opentdb.com/api.php?amount=5&difficulty=medium")
+            const data = await res.json()
+            setQuestions(data.results)
+        }
+        getQuestions()
+    }, [])
+
     return(
         <main>
             {
--- src/quizzical/utils/getQuestions.jsx
+++
@@ -1,6 +0,0 @@
-export default function getQuestions() {
-    fetch('https://opentdb.com/api.php?amount=5&difficulty=medium')
-        .then(response => response.json())
-        .then(data => {return data})
-        .catch(error => console.error(error));
-}
