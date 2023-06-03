c445ffe803c0b8bfc48c34f17da28cd06c44c70d
Deniz Işık <denizisik2004@gmail.com>
2023-06-03 12:18:07 +0300

    Update Intro.jsx

--- src/quizzical/components/Intro.jsx
+++ src/quizzical/components/Intro.jsx
@@ -4,8 +4,8 @@ export default function Intro() {
             <img className="shape-top absolute right-0 w-20vw z-[-1]" src="assets/quizzical/shape-1.png" alt="Shape Top" />
 
             <div className="intro--container flex flex-col justify-center items-center h-screen">
-                <h1 className="font-bold text-3xl">Quizzical</h1>
-                <p>Answer the questions and test your knowledge!</p>
+                <h1 className="font-bold text-3xl pb-2">Quizzical</h1>
+                <p className="pb-2">Answer the questions and test your knowledge!</p>
                 <button className="bg-[#F9A826] text-white font-bold font-['inter'] py-2 px-4 rounded-full mt-4">Start Quiz</button>
             </div>
 
